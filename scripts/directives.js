aa467ca6fd1e51c04c9826e2470019ab268833f8
Lucho Cannavo <luchotc@hotmail.com.ar>
2016-05-04 15:49:02 -0300

    layout changes2

--- scripts/directives.js
+++ scripts/directives.js
@@ -30,16 +30,6 @@ angular.module("app")
     }
 })
 
-.directive('descripcionYDetalle', function(){
-    return {
-        scope:{
-            items: "=",
-            categoria: "@"
-        },
-        templateUrl: 'templates/descripcionYDetalle.html',
-    }
-})
-
 .directive('onlyDesc', function(){
     return {
         scope:{
